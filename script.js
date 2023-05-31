71f8f68056d907ac9b44df2422f396c6f442195a
Lochlan Savage <lochlansavage@live.ca>
2023-05-30 23:36:21 -0700

    adding closing X to burger

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const menuContent = document.getElementById('burger-menu-content');
 burgerIcon.addEventListener('click', () => {
   menuContent.classList.toggle('show');
 });
+
+document.getElementById('burger-icon').addEventListener('click', function() {
+  var icon = this;
+  var menu = document.getElementById('burger-menu-content');
+
+  icon.classList.toggle('active');
+  menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+});
